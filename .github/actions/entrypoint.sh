0f0635082a6263aa97f36422c7fe42564e41ed45
Subhamay Bhattacharyya <subhamay.aws+github+cloudworks@gmail.com>
2025-02-25 13:48:39 -0500

    SB-0001: Initial commit.

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -11,7 +11,7 @@ initialize_json() {
     fi
 }
 
-# Add key-value pair to JSON
+# Add key-value pair to JSON, including nested keys
 add_key_value() {
     local key="$1"
     local value="$2"
@@ -21,16 +21,31 @@ add_key_value() {
         return 1
     fi
 
+    # Check for nested keys using dot notation
+    IFS='.' read -ra keys <<< "$key"
+    jq_script='.'
+    for k in "${keys[@]}"; do
+        jq_script+=" | .\"$k\""
+    done
+
     # Update JSON using jq
-    jq --arg k "$key" --arg v "$value" '. + {($k): $v}' "$JSON_FILE" > temp.json && mv temp.json "$JSON_FILE"
-    echo "Added key: $key with value: $value to $JSON_FILE."
+    jq "$jq_script = \"$value\"" "$JSON_FILE" > temp.json && mv temp.json "$JSON_FILE"
+    echo "Added nested key: $key with value: $value to $JSON_FILE."
 }
 
 # Main execution
 initialize_json
 
-k="testKey"
-v="testValue"
+k="awsLambda"
+v="True"
+if [ ! -z "$k" ] && [ ! -z "$v" ]; then
+    add_key_value "$k" "$v"
+else
+    echo "Usage: $0 [key] [value] or $0 --interactive"
+fi
+
+k="awsLambda.filesModified"
+v="True"
 if [ ! -z "$k" ] && [ ! -z "$v" ]; then
     add_key_value "$k" "$v"
 else
